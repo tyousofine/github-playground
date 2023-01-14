8f4f332126d7ea4f7989922e7c51148c8b70e69b
William <williamw15@protonmail.com>
2023-01-13 21:15:31 -0500

    add Body component to application

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from 'react';
+
+export default class Body extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>this is a test body</h2>
+      </div>
+    );
+  }
+}
